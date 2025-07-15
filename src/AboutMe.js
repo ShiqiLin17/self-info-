9b6db2d90f656273296585e1ff5a78f0d562926b
Shiqi <115582718+ShiqiLin17@users.noreply.github.com>
2025-07-15 15:16:19 -0400

    Create AboutMe.js

---
+++ src/AboutMe.js
@@ -0,0 +1,12 @@
+const AboutMe = () => {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p> 
+        Hi, I'm Shiqi. I am a rising Sophomore at Boston University. I am currently refreshing my memory for Javascript by building my own React app.
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
